5f5f6c0da14582394296fec286ddb4e18ac41507
axiomabsolute <axiomabsolute@gmail.com>
2015-11-08 17:52:16 -0800

    match, (De)indent, append, page up/down

--- init.lua
+++ init.lua
@@ -48,12 +48,91 @@ local function line_append()
     insert_mode()
 end
 
+local function append()
+    insert_mode()
+    buffer:char_right()
+end
+
 local function match_brace()
     local match = buffer:brace_match(buffer.current_pos)
     if (match ~= -1) then
         buffer.current_pos = match
         buffer.set_empty_selection(buffer.current_pos)
     end
+end
+
+local function page_down()
+    buffer:page_down()
+end
+
+local function page_up()
+    buffer:page_up()
+end
+
+local function paste(before)
+    -- TODO: refactor to work with selections
+    -- TODO: refactor to work with line-wise pastes
+    -- fails at start/end of buffer
+    local start = buffer.current_pos
+    local text = ui.clipboard_text
+    local is_line = string.match(text, "^\n.*\n$")
+
+    local pastepos
+    local adjustment = 0
+    if (before) then 
+        adjustment = string.len(text)
+        if (is_line) then
+            -- pastepos is beginning of line
+            insert_mode()
+            buffer:home()
+            pastepos = buffer.current_pos
+        else
+            -- pastepos is before cursor
+            insert_mode()
+            pastepos = buffer.current_pos
+        end
+    else
+        if (is_line) then
+            -- pastepos is end of line
+            append()
+            buffer:line_end()
+            pastepos = buffer.current_pos
+        else
+            -- pastepos is after cursor
+            append()
+            pastepos = buffer.current_pos
+        end
+    end
+
+    buffer:paste()
+    buffer.current_pos = start + adjustment
+    command_mode()
+end
+
+local function indent(back)
+    local currentpos = buffer.current_pos
+    buffer:home()
+    -- TODO: refactor to work with multiple selections
+    -- TODO: refactor to account for Tabs vs spaces
+    -- TODO: refactor to work with selections
+    local adjustment
+    if (back) then
+        buffer:back_tab()
+        adjustment = -2
+    else 
+        buffer:tab()
+        adjustment = 2
+    end
+    buffer.current_pos = currentpos
+    buffer.set_empty_selection(buffer.current_pos + adjustment)
+end
+
+local function undo()
+    buffer:undo()
+end
+
+local function redo()
+    buffer:redo()
 end
 
 local verb_untransitive = {
@@ -65,8 +144,19 @@ local verb_untransitive = {
     ["/"] = find,
     ["^"] = line_home,
     ["$"] = line_end,
-    ["%"] = match_brace,
+    ["m"] = match_brace,
     A = line_append,
+    ["H"] = {undefined, "H"},
+    ["L"] = {undefined, "L"},
+    ["J"] = page_down,
+    ["K"] = page_up,
+    a = append,
+    [">"] = {indent, false},
+    ["<"] = {indent, true},
+    u = undo,
+    U = redo,
+    p = {paste, false},
+    P = {paste, true}
 }
 
 local verb_prepphrase = {
